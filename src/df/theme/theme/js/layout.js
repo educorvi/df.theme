0eb5488ad4306170cd135aeb993a18a22b3f1896
mihai-macaneata <themacaneata@gmail.com>
2016-12-15 16:29:21 +0200

    fixed footer and menu for mobile

--- src/df/theme/theme/js/layout.js
+++ src/df/theme/theme/js/layout.js
@@ -1,7 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+if (window.matchMedia("(max-width: 480px)").matches) {
+
+    $footer_nl_label = $('.footer-newsletter label');
+    $footer_nl_input = $('.footer-newsletter input');
+    $footer = $('#portal-footer');
+
+    $footer_nl_label.addClass('footer_nl');
 
 
 
+
+    $footer.on('click', function(e) {
+        if ($(e.target).hasClass('footer_nl') || $(e.target).hasClass('emailButton') || $(e.target).hasClass('emailInput')) {
+            $footer_nl_label.addClass('nl-hide');
+        } else {
+            $footer_nl_label.removeClass('nl-hide');
+        }
+
+    })
+
+}
 
 
 
