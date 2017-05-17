7412221248418a0510f4ffb9e280ebf1730f81d7
TeamWebHamburg <teamweb@bg-verkehr.de>
2017-05-17 09:17:04 +0200

    17.5.17

--- src/df/theme/theme/js/deutscheflagge.js
+++ src/df/theme/theme/js/deutscheflagge.js
@@ -57,4 +57,12 @@ $(document).ready(function(event) {
     }
 
 
+    // Anpassungen bei einer Fehlerseite
+    $('.template-default_error_message.site-en .banner-text').html('<h1>Mayday <em>+</em></h1>');
+    $('.template-default_error_message.site-en .banner-image').attr('src','http://www-test.deutsche-flagge.de/en/redaktion-englisch/pictures/titelbilder/fehlermeldung-havariekommando.jpg/@@images/image');
+
+    $('.template-default_error_message.site-de .banner-text').html('<h1>Mayday <em>+</em></h1>');
+    $('.template-default_error_message.site-de .banner-image').attr('src','http://www-test.deutsche-flagge.de/de/redaktion/bilder/titelbilder/fehlermeldung-havariekommando.jpg/@@images/image');
+
+
 });
