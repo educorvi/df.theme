b214b11bd5fb16910d576e9fa0d32031ac8b7a7c
TeamWebHamburg <teamweb@bg-verkehr.de>
2017-06-02 10:35:27 +0200

    Bluebox and deutscheflagge.js

--- src/df/theme/theme/js/deutscheflagge.js
+++ src/df/theme/theme/js/deutscheflagge.js
@@ -1,44 +1,6 @@
 $(document).ready(function(event) {
 
 
-    // Bildwechsel beim Mapping - deutsch
-    var seite = String(window.location).search(/organigramme/);
-
-    if (seite != -1)
-
-    {
-
-        $('#content-core area').mouseenter(function() {
-            var bildsrc = $(this).attr('id') + '_ds.jpg';
-            $("#content-core img").attr("src", bildsrc);
-        });
-        $('#content-core area').mouseleave(function() {
-            $("#content-core img").attr("src","organigramm_ds.jpg");
-        });
-
-
-    };
-
-
-    // Bildwechsel beim Mapping - englisch
-    var seite = String(window.location).search(/organigrams/);
-
-    if (seite != -1)
-
-    {
-
-        $('#content-core area').mouseenter(function() {
-            var bildsrc = $(this).attr('id') + '_ds.jpg';
-            $("#content-core img").attr("src", bildsrc);
-        });
-        $('#content-core area').mouseleave(function() {
-            $("#content-core img").attr("src","organigram_ds.jpg");
-        });
-
-
-    };
-
-
     // Anpassungen der Sucheinstellungen
     $('#search-filter-toggle').hide();
     $('cite').hide();
@@ -60,9 +22,11 @@ $(document).ready(function(event) {
     // Anpassungen bei einer Fehlerseite
     $('.template-default_error_message.site-en .banner-text').html('<h1>Mayday <em>+</em></h1>');
     $('.template-default_error_message.site-en .banner-image').attr('src','http://www.deutsche-flagge.de/en/redaktion-englisch/pictures/titelbilder/fehlermeldung-havariekommando.jpg/@@images/image');
+    $('.template-default_error_message.site-en .banner-image').attr('title','© Havariekommando www.havariekommando.de');
 
     $('.template-default_error_message.site-de .banner-text').html('<h1>Mayday <em>+</em></h1>');
     $('.template-default_error_message.site-de .banner-image').attr('src','http://www.deutsche-flagge.de/de/redaktion/bilder/titelbilder/fehlermeldung-havariekommando.jpg/@@images/image');
+    $('.template-default_error_message.site-de .banner-image').attr('title','© Havariekommando www.havariekommando.de');
 
 
 });
