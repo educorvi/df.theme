814ed6dbdc229fd506993fd687c6cacb68a3f399
astehr <andreas.stehr@bg-verkehr.de>
2019-06-25 13:23:43 +0200

    http nach https

--- src/df/theme/theme/js/deutscheflagge.js
+++ src/df/theme/theme/js/deutscheflagge.js
@@ -11,8 +11,8 @@ $(document).ready(function(event) {
 
         {
         $('#parent-fieldname-text').after('<div id="ajaxcontent">Content-Core</div>');
-        var url = 'http://www-prod.deutsche-flagge.de/de/sicherheit/piraterie/warnhinweise/warnhinweise/CookedBody'
-        //var url = 'http://www.deutsche-flagge.de/optiflag/de/sicherheit/piraterie/warnhinweise/warnhinweise/CookedBody'
+        var url = 'https://www-prod.deutsche-flagge.de/de/sicherheit/piraterie/warnhinweise/warnhinweise/CookedBody'
+        //var url = 'https://www.deutsche-flagge.de/optiflag/de/sicherheit/piraterie/warnhinweise/warnhinweise/CookedBody'
         $('#ajaxcontent').load(url);
 
         $.ajaxSetup({cache: false});
@@ -55,15 +55,15 @@ $(document).ready(function(event) {
     // Anpassungen bei einer Fehlerseite
     $('.template-default_error_message.site-optiflag #design-credits').before('<div class="df-theme-banner"><img class="banner-image"><div class="banner-text"></div></div>');
     $('.template-default_error_message.site-optiflag .banner-text').html('<h1>Mayday <em>+</em></h1>');
-    $('.template-default_error_message.site-optiflag .banner-image').attr('src','http://www.deutsche-flagge.de/en/redaktion-englisch/pictures/titelbilder/fehlermeldung-havariekommando.jpg/@@images/image');
+    $('.template-default_error_message.site-optiflag .banner-image').attr('src','https://www.deutsche-flagge.de/en/redaktion-englisch/pictures/titelbilder/fehlermeldung-havariekommando.jpg/@@images/image');
     $('.template-default_error_message.site-optiflag .banner-image').attr('title','© Havariekommando www.havariekommando.de');
 
     $('.template-default_error_message.site-en .banner-text').html('<h1>Mayday <em>+</em></h1>');
-    $('.template-default_error_message.site-en .banner-image').attr('src','http://www.deutsche-flagge.de/en/redaktion-englisch/pictures/titelbilder/fehlermeldung-havariekommando.jpg/@@images/image');
+    $('.template-default_error_message.site-en .banner-image').attr('src','https://www.deutsche-flagge.de/en/redaktion-englisch/pictures/titelbilder/fehlermeldung-havariekommando.jpg/@@images/image');
     $('.template-default_error_message.site-en .banner-image').attr('title','© Havariekommando www.havariekommando.de');
 
     $('.template-default_error_message.site-de .banner-text').html('<h1>Mayday <em>+</em></h1>');
-    $('.template-default_error_message.site-de .banner-image').attr('src','http://www.deutsche-flagge.de/de/redaktion/bilder/titelbilder/fehlermeldung-havariekommando.jpg/@@images/image');
+    $('.template-default_error_message.site-de .banner-image').attr('src','https://www.deutsche-flagge.de/de/redaktion/bilder/titelbilder/fehlermeldung-havariekommando.jpg/@@images/image');
     $('.template-default_error_message.site-de .banner-image').attr('title','© Havariekommando www.havariekommando.de');
 
 
