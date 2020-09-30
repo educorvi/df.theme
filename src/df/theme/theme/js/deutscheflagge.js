756d1bf81423bee32cafefccb116f6eab59dfa87
astehr <andreas.stehr@bg-verkehr.de>
2020-09-30 10:05:42 +0200

    Anpassungen Ende September

--- src/df/theme/theme/js/deutscheflagge.js
+++ src/df/theme/theme/js/deutscheflagge.js
@@ -30,7 +30,7 @@ $(document).ready(function(event) {
 
 
     // Anpassungen bei einer Fehlerseite
-    $('.template-default_error_message.site-optiflag #design-credits').before('<div class="df-theme-banner"><img class="banner-image"><div class="banner-text"></div></div>');
+    $('.template-default_error_message.site-optiflag #portal-breadcrumbs').before('<div class="df-theme-banner"><img class="banner-image"><div class="banner-text"></div></div>');
     $('.template-default_error_message.site-optiflag .banner-text').html('<div class="bildtitel">Mayday <em>+</em></div>');
     $('.template-default_error_message.site-optiflag .banner-image').attr('src','https://www.deutsche-flagge.de/en/redaktion-englisch/pictures/titelbilder/fehlermeldung-havariekommando.jpg/@@images/image');
     $('.template-default_error_message.site-optiflag .banner-image').attr('title','© Havariekommando www.havariekommando.de');
@@ -42,6 +42,22 @@ $(document).ready(function(event) {
     $('.template-default_error_message.site-de .banner-text').html('<div class="bildtitel">Mayday <em>+</em></div>');
     $('.template-default_error_message.site-de .banner-image').attr('src','https://www.deutsche-flagge.de/de/redaktion/bilder/titelbilder/fehlermeldung-havariekommando.jpg/@@images/image');
     $('.template-default_error_message.site-de .banner-image').attr('title','© Havariekommando www.havariekommando.de');
+
+    // Anpassungen bei der Suchseite
+    $('.template-search.site-optiflag #searchform').before('<div class="df-theme-banner"><img class="banner-image"><div class="banner-text"></div></div>');
+    $('.template-search.site-optiflag .banner-text').html('<div class="bildtitel">PEILUNG <em>+</em></div><div class="bildtitel">HABEN</div>');
+    $('.template-search.site-optiflag .banner-image').attr('src','https://www.deutsche-flagge.de/en/redaktion-englisch/pictures/titelbilder/header-kontakt.jpg/@@images/image');
+    $('.template-search.site-optiflag .banner-image').attr('title','© Kirk Williams');
+
+    $('.template-search.site-en #searchform').before('<div class="df-theme-banner"><img class="banner-image"><div class="banner-text"></div></div>');
+    $('.template-search.site-en .banner-text').html('<div class="bildtitel">GIVING <em>+</em></div><div class="bildtitel">ORIENTATION</div>');
+    $('.template-search.site-en .banner-image').attr('src','https://www.deutsche-flagge.de/en/redaktion-englisch/pictures/titelbilder/header-kontakt.jpg/@@images/image');
+    $('.template-search.site-en .banner-image').attr('title','© Kirk Williams');
+
+    $('.template-search.site-de #searchform').before('<div class="df-theme-banner"><img class="banner-image"><div class="banner-text"></div></div>');
+    $('.template-search.site-de .banner-text').html('<div class="bildtitel">PEILUNG <em>+</em></div><div class="bildtitel">HABEN</div>');
+    $('.template-search.site-de .banner-image').attr('src','https://www.deutsche-flagge.de/de/redaktion/bilder/titelbilder/header-kontakt.jpg/@@images/image');
+    $('.template-search.site-de .banner-image').attr('title','© Kirk Williams');
 
 
 });
